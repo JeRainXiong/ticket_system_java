9442e3b388f87229d9cec1877a746af83805e72b
eggTart007 <514160451@qq.com>
2019-01-05 09:17:53 +0800

    日期格式化

--- src/com/whut/controller/ContentController.java
+++ src/com/whut/controller/ContentController.java
@@ -1,6 +1,8 @@
 package com.whut.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,27 +18,32 @@ import com.whut.service.impl.TicketServiceImpl;
 
 @Controller
 public class ContentController extends BaseController {
-	@Autowired
-	ConcertServiceImpl concertServiceImpl;
-	@Autowired
-	TicketServiceImpl ticketServiceImpl;
-	
-	@RequestMapping("/content")
-    public ModelAndView content(int concertId,Model model){     
-	   
-	   
-	   Concert thisConcert = concertServiceImpl.getById(concertId);
-	   List<TicketType> ticketTypeList= new ArrayList<TicketType>();
-	   ticketTypeList=ticketServiceImpl.getTicketTypeListByConcert(concertId);
-//       System.out.println("100"+ticketTypeList);
-	   if(thisConcert==null)
-		   return showMessage("未查询到该演唱会",model);
-	   model.addAttribute("ticketTypeList",ticketTypeList);
-	   System.out.println(ticketTypeList);
-	   model.addAttribute("concert", thisConcert);
-	   model.addAttribute("title", thisConcert.getConcertName());
-       return new ModelAndView("content");
+    @Autowired
+    ConcertServiceImpl concertServiceImpl;
+    @Autowired
+    TicketServiceImpl ticketServiceImpl;
+
+    @RequestMapping("/content")
+    public ModelAndView content(int concertId, Model model) {
+
+        Concert thisConcert = concertServiceImpl.getById(concertId);
+        // 修改日期显示格式
+        Date date = thisConcert.getConcertTime();
+        String pattern = "yyyy-MM-dd HH-mm-ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        String datestr = sdf.format(date);// format 为格式化方法
+
+        List<TicketType> ticketTypeList = new ArrayList<TicketType>();
+        ticketTypeList = ticketServiceImpl.getTicketTypeListByConcert(concertId);
+        // System.out.println("100"+ticketTypeList);
+        if (thisConcert == null)
+            return showMessage("未查询到该演唱会", model);
+        model.addAttribute("ticketTypeList", ticketTypeList);
+        // System.out.println(ticketTypeList);
+        model.addAttribute("concert", thisConcert);
+        model.addAttribute("concertTime", datestr);
+        model.addAttribute("title", thisConcert.getConcertName());
+        return new ModelAndView("content");
     }
-	
-	
+
 }
